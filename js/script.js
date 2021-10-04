a626a0902c57063a9ffcf01c2bd0f911054415f1
Montoya <MontoyaMontes@github.com>
2021-10-04 17:21:05 -0500

    Add new pages (documents, gallery) Features: documents.html almost finished, gallery improved basic logic To do: Add new pictures in gallery, fix bug in gallery and add new pages (proyectos, cursos) By: Montoya M

--- js/script.js
+++ js/script.js
@@ -8,12 +8,35 @@ const navMenu = document.querySelector(".navMenu");
 const navigation = document.querySelector(".navigation");
 
 
+
+
 /* MENU SHOW*/
 
 navMenu.addEventListener("click", ()=> {
 	navMenu.classList.toggle("active");
-	navigation.classList.toggle("active");	
+	navigation.classList.toggle("active");
 });
+
+
+cta1.addEventListener("click", ()=> {
+		more1.classList.toggle("hidden");
+})
+
+
+cta2.addEventListener("click", ()=> {
+		more2.classList.toggle("hidden");
+})
+
+/*
+const more1 = document.querySelector(".pdfContainer1"),
+		  cta1 = document.querySelector(".cta1");
+		  
+const ctaClose = document.querySelector(".ctaClose");
+
+ctaClose.addEventListener("click", ()=> {
+		more.classList.toggle("hidden");
+})
+*/
 
 /*MENU CLOSE WHEN CLICK*/
 
@@ -41,4 +64,33 @@ window.addEventListener("scroll", function(){
 	const header = document.querySelector('header');
 	header.classList.toggle("sticky",window.scrollY>0);
 })
+
+/* ============== GALLERY ==============*/
+
+// Selecting all the images.
+const images = document.querySelectorAll("img");
+
+// The API options: when 20% of the image is visible.
+const imgOptions = {
+  threshold: 0.2
+};
+
+// Calling the API constructor.
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    // If the image is not visible.
+    if (!entry.isIntersecting) return;
+    
+    // If it's visible.
+    const img = entry.target;
+    img.src = img.src.replace("w=10&", "w=800&");
+    imgObserver.unobserve(entry.target);
+  });
+}, imgOptions);
+
+// Observing the images.
+images.forEach((img) => {
+  imgObserver.observe(img);
+});
+
 
